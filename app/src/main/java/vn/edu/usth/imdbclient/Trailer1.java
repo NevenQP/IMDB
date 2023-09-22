6bf7e01d90ec012651c4a3725eebe13b6e76863b
NevenQP <phuongtt.bi12-353@st.usth.edu.vn>
2023-09-22 10:46:53 +0700

    5

---
+++ app/src/main/java/vn/edu/usth/imdbclient/Trailer1.java
@@ -0,0 +1,29 @@
+package vn.edu.usth.imdbclient;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Trailer1 extends Fragment {
+    public Trailer1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_trailer1, container, false);
+        return view;
+    }
+}
