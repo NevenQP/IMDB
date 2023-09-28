45a5f6312989a960a0a4d4c70d2041dfa1628b35
Nguyen Dung <dungng.bi12-110@st.usth.edu.vn>
2023-09-28 20:39:18 +0700

    error check

--- app/src/main/java/vn/edu/usth/imdbclient/Search.java
+++ app/src/main/java/vn/edu/usth/imdbclient/Search.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import vn.edu.usth.imdbclient.Models.MovieModel;
+import vn.edu.usth.imdbclient.reponse.MovieSearchResponse;
+import vn.edu.usth.imdbclient.request.Servicey;
+import vn.edu.usth.imdbclient.utils.Credentials;
+import vn.edu.usth.imdbclient.utils.MovieApi;
+
 public class Search extends Fragment {
+    Button btn;
+
     public Search() {
         // Required empty public constructor
     }
@@ -20,8 +37,47 @@ public class Search extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-
         // Inflate the layout for this fragment
+        btn = btn.findViewById(R.id.search_button_1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetRetrofitResponse();
+            }
+        });
+
         return view;
+    }
+
+    private void GetRetrofitResponse() {
+        MovieApi movieApi = Servicey.getMovieApi();
+        Call<MovieSearchResponse> responseCall = movieApi
+                .searchMovie(
+                        Credentials.API_KEY,
+                        "Loki",
+                        "1");
+        responseCall.enqueue(new Callback<MovieSearchResponse>() {
+            @Override
+            public void onResponse(Call<MovieSearchResponse> call, Response<MovieSearchResponse> response) {
+                if (response.code() == 200) {
+                    Log.v("Tag", "the response" + response.body().toString());
+                    List<MovieModel> movies = new ArrayList<>(response.body().getMovies());
+                    for (MovieModel movie : movies) {
+                        Log.v("Tag", "The Release Date" + movie.getRelease_date());
+                    }
+                } else {
+                    try {
+                        Log.v("Tag", "Error" + response.errorBody().string());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieSearchResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
